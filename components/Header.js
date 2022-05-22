92dfff930093338faae430c400b97538eb11f67f
muhib-giki <muhibmhanif@gmail.com>
2022-05-23 01:47:15 +0500

    Trending Card component is done

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const styles = {
   headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
   nav: `flex justify-center items-center gap-[20px]`,
   navItem: `relative mr-1 cursor-pointer hover:opacity-60`,
-  navLink: ``,
+  navLink: `text-white flex mx-[10px]`,
   badge: `rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`,
   inputContainer: `flex items-center justify-center p-2 rounded bg-[#171924]`,
   input: `bg-transparent outline-none text-white w-70 ml-3`,
@@ -25,28 +25,28 @@ const Header = () => {
       <div className={styles.headerWrapper}>
         <nav className={styles.nav}>
           <div className={styles.navItem}>
-            <div className={styles.navLink}>cryptocurrencies</div>
+            <div className={styles.navLink}>Cryptocurrencies</div>
             <div className={styles.badge} />
           </div>
           <div className={styles.navItem}>
-            <div className={styles.navLink}>exchanges</div>
+            <div className={styles.navLink}>Exchanges</div>
           </div>
           <div className={styles.navItem}>
             <div className={styles.navLink}>NFT</div>
             <div className={styles.badge} />
           </div>
           <div className={styles.navItem}>
-            <div className={styles.navLink}>cryptown</div>
+            <div className={styles.navLink}>Cryptown</div>
             <div className={styles.badge} />
           </div>
           <div className={styles.navItem}>
-            <div className={styles.navLink}>portfolio</div>
+            <div className={styles.navLink}>Portfolio</div>
           </div>
           <div className={styles.navItem}>
-            <div className={styles.navLink}>watchlist</div>
+            <div className={styles.navLink}>Watchlist</div>
           </div>
           <div className={styles.navItem}>
-            <div className={styles.navLink}>products</div>
+            <div className={styles.navLink}>Products</div>
             <div className={styles.badge} />
           </div>
           <div className={styles.navItem}>
--- components/Trending.js
+++ components/Trending.js
@@ -7,10 +7,39 @@ import gainers from '../assets/gainers.png'
 import recent from '../assets/recent.png'
 import ReactSwitch from 'react-switch'
 import Rate from '../components/cmc-table/Rate'
+import TrendingCard from './TrendingCard'
 const styles = {
-  trendingWrapper: `mx-auto max-w-screen-2xl`,
-  h1: `text-3xl text-black`,
+  trendingWrapper: `mx-auto max-w-screen-xl`,
+  h1: `text-3xl text-white`,
+  flexCenter: `flex items-center`,
 }
+
+const trendingData = [
+  {
+    number: 1,
+    symbol: 'BTC',
+    name: 'Bitcoin',
+    icon: btc,
+    isIncrement: true,
+    rate: '2.34%',
+  },
+  {
+    number: 2,
+    symbol: 'USDT',
+    name: 'Tether',
+    icon: usdt,
+    isIncrement: false,
+    rate: '9.23%',
+  },
+  {
+    number: 3,
+    symbol: 'BTC',
+    name: 'Bitcoin',
+    icon: btc,
+    isIncrement: true,
+    rate: '2.34%',
+  },
+]
 
 const Trending = () => {
   const [checked, setChecked] = useState(false)
@@ -44,6 +73,23 @@ const Trending = () => {
           </p>
         </div>
         <br />
+        <div className={styles.flexCenter}>
+          <TrendingCard
+            title="Trending"
+            icon={fire}
+            trendingData={trendingData}
+          />
+          <TrendingCard
+            title="Biggest Gainers"
+            icon={gainers}
+            trendingData={trendingData}
+          />
+          <TrendingCard
+            title="Recently Added"
+            icon={recent}
+            trendingData={trendingData}
+          />
+        </div>
       </div>
     </div>
   )
